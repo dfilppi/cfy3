a9ccf71fd9d410b140354a0c8eba7b6ec3bf94ea
DeWayne Filppi <dewayne@gigaspaces.com>
2015-05-23 16:11:44 -0700

    fixes for getting mongo url propertly

--- dproxy/example/nodejs/scripts/mongo/set-mongo-url.sh
+++ dproxy/example/nodejs/scripts/mongo/set-mongo-url.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties outputs.server_info.value.ip_address)
-ctx source instance runtime_properties mongo_port $(ctx target instance runtime_properties outputs.server_info.value.port)
+ctx source instance runtime_properties mongo_ip_address $(ctx target instance runtime_properties outputs.endpoint.value.ip_address)
+ctx source instance runtime_properties mongo_port $(ctx target instance runtime_properties outputs.endpoint.value.port)
+
